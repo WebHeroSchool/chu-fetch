6ec415d024daa114a9c5457927644a36e79c780b
ChuVo <cahek764@ya.ru>
2019-02-17 23:07:36 +0300

    added lack of information

--- js/fetch.js
+++ js/fetch.js
@@ -1,40 +1,54 @@
 let log = console.log; 
 // let user = prompt('enter a username for search');
-let userName = '6thSence';//for exampl
+let userName = '6thSence';
+	userName = 'Chuvo'//for exampl
 document.getElementById('search-user').addEventListener('click', function() {
-	// userName = document.getElementById('nickname-value').value;
-	log('user name' + userName);
+	userName = document.getElementById('nickname-value').value;
+	log('userName for search = ' + userName);
 	searchUser (userName);
 })
 
 function searchUser () {	
 	let nameLink = document.getElementById('name-link');
 	fetch(`https://api.github.com/users/${userName}`)
-		.then( result => result.json())
-		.then( function getName(json) {
-			let name = json.name;
+		.then( function getResponse(response) {
+			return response.json()
+			//добавить обработку ошибки
+		})
+
+		.then( function getName(json) {			
+			let name = json.name;		
 			nameLink.innerHTML = name;
-			log(json.name);		
-			return json
+			log('nickName = ' + json.name);		
+			return json 			
 		})	
+
+		.then(function getLink(json) {
+			let url = json.html_url;
+			nameLink.href= url;
+			return json
+		})
+
 		.then( function getAvater(json) {
 			log(json.avatar_url);
-			let imgElem = document.getElementById('avatar-user');
-			let avatarUrl = json.avatar_url;
+			let imgElem = document.getElementById('avatar-user'),
+				avatarUrl = json.avatar_url;
 			imgElem.style.backgroundImage =  `url(${avatarUrl}`;
 			return json				
 		})
+
 		.then( function getBio(json) {
-			let infoUser = json.bio.toString().split('\r\n\r\n').join(',<br>');
-			log('split= '+json.bio);
-			let info = document.getElementById('info');
-			info.innerHTML=infoUser;
-			return json
-		})	
-		.then(function getLink(json) {
-			let url = json.html_url;
-			nameLink.href= url
-		})	
+			let infoUser = json.bio;
+			log('split= ' + json.bio);
+			if (infoUser !=null) {
+				infoUser.toString().split('\r\n\r\n').join(',<br>');
+				let info = document.getElementById('info');
+				info.innerHTML = infoUser;
+				return json
+			} else {
+				info.innerHTML ='информации о пользователе нет';
+			}			
+		})			
 }
 	
 
