6a4487c23e5333b6d4445f6b9bce6fa6666278d4
ChuVo <cahek764@ya.ru>
2019-02-22 19:05:59 +0300

    updated files

--- js/fetch.js
+++ js/fetch.js
@@ -1,70 +1,59 @@
+const getDataButton = document.querySelector('.get-data-button');
 
-let param = new URLSearchParams(window.location.search);
-let userName1 = param.get('username');
-let url = 'https://api.github.com/users/';
-let request = url + userName1;
+getDataButton.addEventListener('click', getData);
 
+function getData() {
+    const searchParams = new URLSearchParams(window.location.search),
+        userName = searchParams.get('username'),
+        request = `https://api.github.com/users/${userName}`;
+        dataContainer = document.querySelector(".data-container"),
+        dataWrapper = document.querySelector(".data-wrapper");
+        
+        
+    fetch(request)
+        .then(response => {
+            if (response.status !==200) {
+                dataContainer.innerHTML = 'Такого пользователя не существует';
+            } else {
+                return response.json(); 
+            }                      
+        })
+        .catch(err => {
+            dataContainer.innerHTML = "что-то пошло не так";
+            console.log(err);
+        })
+        .then(json => {
+            const userName = json.name,
+                  printName = document.createElement('a');
 
+            printName.innerHTML = `Имя пользователя: ${userName}`; 
+            printName.classList.add('print-name'); 
+            printName.classList.add('render');
+            document.querySelector(".data-wrapper").appendChild(printName).href = json.html_url;                
 
+            console.log(`Имя пользователя: ${json.name}`);
+            return json;
+        })
+        .then(json => {
+            const printAvatar = document.createElement('img');
+                
 
+            dataContainer.insertBefore(printAvatar, dataWrapper).src = json.avatar_url;
+            printAvatar.classList.add("avatar");
+            console.log(json);
+            return json;
+        })
+        
+        .then(json => {
+            const getBio = json.bio,
+                  printBio = document.createElement('div');
 
-// let userName = '6thSence';
-// 	userName = 'Chuvo'//for exampl
-document.getElementById('search-user').addEventListener('click', function() {
-	userName = document.getElementById('nickname-value').value;
-	// log('userName for search = ' + userName);
-	searchUser ();
-})
-
-function searchUser () {	
-	let nameLink = document.getElementById('name-link');
-	fetch(request)
-		.then( function getResponse(response) {
-			return response.json()
-			//добавить обработку ошибки
-		})
-
-		.then( function getName(json) {			
-			let name = json.name;		
-			nameLink.innerHTML = name;
-			log('nickName = ' + json.name);		
-			return json 			
-		})	
-
-		.then(function getLink(json) {
-			let url = json.html_url;
-			nameLink.href= url;
-			return json
-		})
-
-		.then( function getAvater(json) {
-			log(json.avatar_url);
-			let imgElem = document.getElementById('avatar-user'),
-				avatarUrl = json.avatar_url;
-			imgElem.style.backgroundImage =  `url(${avatarUrl}`;
-			return json				
-		})
-.then( json => {
-			log(json);
-			document.getElementById('data').innerHTML=json;
-			return json
-
-		})		
-		.then( function getBio(json) {
-			let infoUser = json.bio;
-			log('split= ' + json.bio);
-			if (infoUser !=null) {
-				infoUser.toString().split('\r\n\r\n').join(',<br>');
-				let info = document.getElementById('info');
-				info.innerHTML = infoUser;
-				return json
-			} else {
-				info.innerHTML ='информации о пользователе нет';
-				return	json
-			}	
-			return json
-		})
-			
+            printBio.classList.add('render');
+            printBio.innerHTML = `
+                <p class = "print-bio">${getBio}</p>
+            `;
+            document.querySelector(".data-wrapper").appendChild(printBio);
+        })
+        
+  
 }
-	
-
